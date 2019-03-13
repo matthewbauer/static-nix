09eb920788112654fbabd6dce351734bc463f97a
Matthew Bauer <mjbauer95@gmail.com>
2019-03-13 18:18:55 -0400

    Initial commit

---
+++ default.nix
@@ -0,0 +1,83 @@
+{ nixpkgsFun ? import (builtins.fetchTarball "https://github.com/matthewbauer/nixpkgs/archive/d65584edcde7c5c4862116b2dbd77862157db061.tar.gz") }:
+
+let
+
+  archs = [ "x86_64" "aarch64" ]; # "armv6l" "armv7l"
+  nativePkgs = nixpkgsFun {};
+
+  nixes = builtins.listToAttrs (map (arch: {
+    name = arch;
+    value = let pkgs = nixpkgsFun {
+      crossOverlays = [ (import "${nativePkgs.path}/pkgs/top-level/static.nix") ];
+      crossSystem = {
+        # busybox just supports gcc!
+        # useLLVM = true;
+
+        config = if arch == "armv6l" || arch == "armv7l" then "${arch}-unknown-linux-musleabi"
+                 else "${arch}-unknown-linux-musl";
+      };
+    }; in nativePkgs.runCommand "nix-${arch}" {
+      nativeBuildInputs = [ nativePkgs.haskellPackages.arx ];
+      passthru = {
+        inherit (pkgs) nix;
+        test = nativePkgs.writeScriptBin "nix" ''
+          ${pkgs.hostPlatform.emulator nativePkgs} ${pkgs.nix}/bin/nix "$@"
+        '';
+      };
+    } ''
+      cp -r ${pkgs.nix}/share share
+      chmod -R 755 share
+      rm -rf share/nix/sandbox
+      chmod 644 share/nix/corepkgs/*
+
+      cp ${pkgs.nix}/bin/nix nix
+      chmod 755 nix
+
+      tar cfz nix.tar.gz nix share/
+
+      mkdir -p $out/libexec/
+      cp ${pkgs.nix}/bin/nix $out/libexec/nix-${arch}
+
+      mkdir -p $out/bin/
+      arx tmpx ./nix.tar.gz -o $out/bin/nix-${arch} // ${nix-runner}
+      chmod +x $out/bin/nix-${arch}
+    '';
+  }) archs);
+
+  nix-runner = nativePkgs.writeScript "nix-runner.sh" ''
+    export NIX_DATA_DIR=$PWD/share
+    arch=$(uname -m)
+    case $arch in
+      i*86) arch=i686 ;;
+      amd64) arch=x86_64 ;;
+      armv6|armv7) arch=''${arch}l ;;
+    esac
+    if [ -x ./nix-$arch ]; then
+      ./nix-$arch "$@"
+    else
+      ./nix "$@"
+    fi
+  '';
+
+  all = nativePkgs.buildEnv {
+    name = "static-nix";
+    paths = builtins.attrValues nixes;
+    passthru = nixes;
+    buildInputs = [ nativePkgs.haskellPackages.arx ];
+    postBuild = ''
+      cp -r ${nixes.x86_64.nix}/share share
+      chmod -R 755 share
+      rm -rf share/nix/sandbox
+      chmod 644 share/nix/corepkgs/*
+
+      cp $out/libexec/* .
+
+      tar cfz nix.tar.gz nix-* share/
+
+      mkdir -p $out/bin/
+      arx tmpx ./nix.tar.gz -o $out/bin/nix // ${nix-runner}
+      chmod +x $out/bin/nix
+    '';
+  };
+
+in all
---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+result=$(nix-build)
+
+dir=$(mktemp -d)
+
+setup() {
+  git clone ssh://git@github.com/matthewbauer/matthewbauer.github.io.git $dir
+  pushd $dir
+}
+
+cleanup() {
+  popd
+  rm -rf $dir
+}
+
+setup
+trap cleanup EXIT
+
+cp $result/bin/* .
+git add nix-*
+git commit -m "Update Nix binaries"
+git push
